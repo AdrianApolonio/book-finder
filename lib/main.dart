1f4834704b2bc2f221ba3f1c414a58c04a37955e
Adrian Apolonio <adrianapolonio17@gmail.com>
2024-03-07 23:06:19 -0800

    Removed need for env. Update pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 import 'pages/auth_page.dart';
 
@@ -9,7 +8,6 @@ Future main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
